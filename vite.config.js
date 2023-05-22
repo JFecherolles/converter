f1b7415997724c77fbfa1c1832fd59805e0461c7
JFecherolles <fecherolles.jerome@outlook.fr>
2023-05-22 17:51:53 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: false,
+  root: './',
+  publicDir: 'public',
+  envDir: '.',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+  },
+  base: '/converter/',
 })
